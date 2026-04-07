5869b39434b1c79296db2bc06d54bcb44df9f1d8
Jakob Staudinger <jakobstaudi@gmail.com>
2026-04-07 22:54:41 +0200

    feat: implement array expressions

--- src/expressions/array.expression.ts
+++ src/expressions/array.expression.ts
@@ -29,4 +29,38 @@ export type ArrayExpression =
         start?: number,
         end?: number
       ) => number;
+    }
+  | { $isArray: <T>(value: T) => boolean }
+  | { $lastN: <T>(input: StaticInput<{ input: T[]; n: number }>) => T[] }
+  | {
+      $map: <T, R>(input: StaticInput<{ input: T[]; as?: string; in: R }>) => R;
+    }
+  | { $maxN: <T>(input: StaticInput<{ input: T[]; n: number }>) => T[] }
+  | { $minN: <T>(input: StaticInput<{ input: T[]; n: number }>) => T[] }
+  | { $objectToArray: (object: object) => { k: string; v: unknown }[] }
+  | { $range: (start: number, end: number, step?: number) => number[] }
+  | {
+      $reduce: <T, R>(
+        input: StaticInput<{ input: T[]; initialValue: R; in: R }>
+      ) => R;
+    }
+  | { $reverseArray: <T>(array: T[]) => T[] }
+  | { $size: <T>(array: T[]) => number }
+  | { $slice: <T>(array: T[], n: number) => T[] }
+  | {
+      $slice: <T>(array: T[], position: number, n: number) => T[];
+    }
+  | {
+      $sortArray: <T>(
+        input: StaticInput<{ input: T[]; sortBy: object | number }>
+      ) => T[];
+    }
+  | {
+      $zip: (
+        input: StaticInput<{
+          inputs: unknown[][];
+          useLongestLength?: boolean;
+          defaults?: unknown[];
+        }>
+      ) => unknown[];
     };

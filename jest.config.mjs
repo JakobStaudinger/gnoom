d3928ee11b8c4f5c04b19e6f1a215671bdbc5022
Jakob Staudinger <jakobstaudi@gmail.com>
2026-04-19 13:04:42 +0200

    chore: enable type-checking in tests

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,10 +1,12 @@
 export default {
   testEnvironment: 'node',
   transform: {
-    '\\.ts$': ['ts-jest', { tsconfig: './tsconfig.spec.json' }]
+    '\\.ts$': [
+      'ts-jest',
+      { tsconfig: './tsconfig.spec.json', diagnostics: { warnOnly: false } }
+    ]
   },
   extensionsToTreatAsEsm: ['.ts'],
-
   moduleNameMapper: {
     '^(.*)\\.js$': '$1'
   }

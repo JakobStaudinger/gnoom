8922bd585a278f774d4a88a772504fc2faa5a4d8
Jakob Staudinger <jakobstaudi@gmail.com>
2026-05-10 12:28:46 +0200

    feat: overload $cond

--- src/expressions/operators/conditional/$cond.ts
+++ src/expressions/operators/conditional/$cond.ts
@@ -1,7 +1,18 @@
+import {
+  Overload,
+  OverloadTransformation,
+  UnknownOverloaded
+} from '../../../types/overload';
 import { StaticInput } from '../../static-input';
 
 export interface $cond {
-  $cond:
-    | (<T, F>(input: StaticInput<{ if: boolean; then: T; else: F }>) => T | F)
-    | (<T, F>(_if: boolean, then: T, _else: F) => T | F);
+  $cond: Overload<UnknownOverloaded, Signature>;
 }
+
+interface Signature extends OverloadTransformation {
+  output:
+    | ((input: Input<this['T']>) => this['T'])
+    | ((_if: boolean, _then: this['T'], _else: this['T']) => this['T']);
+}
+
+type Input<T> = StaticInput<{ if: boolean; then: T; else: T }>;

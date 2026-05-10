b01d1336ccd011b226edbe1d28656a429b6c4bf7
Jakob Staudinger <jakobstaudi@gmail.com>
2026-05-10 12:29:04 +0200

    feat: overload $switch

--- src/expressions/operators/conditional/$switch.ts
+++ src/expressions/operators/conditional/$switch.ts
@@ -1,10 +1,19 @@
+import {
+  Overload,
+  OverloadTransformation,
+  UnknownOverloaded
+} from '../../../types/overload';
 import { StaticInput } from '../../static-input';
 
 export interface $switch {
-  $switch: <T>(
-    input: StaticInput<{
-      branches: { case: boolean; then: T }[];
-      default?: T;
-    }>
-  ) => T;
+  $switch: Overload<UnknownOverloaded, Signature>;
 }
+
+interface Signature extends OverloadTransformation {
+  output: (input: Input<this['T']>) => this['T'];
+}
+
+type Input<T> = StaticInput<{
+  branches: { case: boolean; then: T }[];
+  default?: T;
+}>;

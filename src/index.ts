13542186c64097807944f32b5fe2b2a4995fd616
Jakob Staudinger <jakobstaudi@gmail.com>
2026-06-02 09:02:19 +0200

    feat: export WithoutFunctions

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export type { QueryPredicate } from './query-predicates';
 export type { InitialState as State } from './types/aggregate-state';
+export type { WithoutFunctions } from './types/without-functions';
 
 export * from './aggregate';
